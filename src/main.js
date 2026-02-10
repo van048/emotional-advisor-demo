f985823e1566b9755dd0f8ba5c3cedcb3f7e86b1
yingbin.yang@midea.com <yingbin.yang@midea.com>
2026-02-10 16:57:54 +0800

    报错

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import router from './router'
+import Toast from 'vue-toastification'
+import 'vue-toastification/dist/index.css'
+
+// Register toast plugin
+Vue.use(Toast)
 
 Vue.config.productionTip = false
 
